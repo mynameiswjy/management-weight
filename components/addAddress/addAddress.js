aca94c5d97d3d3f647fc8ef13d84ee9ca26157fd
wjy <wjy631438519@163.com>
2020-04-14 22:31:27 +0800

    commit

--- components/addAddress/addAddress.js
+++ components/addAddress/addAddress.js
@@ -10,24 +10,66 @@ Component({
   },
   data: {
     province: [],
-    selectIndex: [0, 0, 0]
+    provinceData: [],
+    cityCode: '',
+    districtCode: ''
   },
   attached() {
     /*this.initData()*/
-    this.cityData()
+    this.provinceData();
   },
   methods: {
     bindchange(e) {
       console.log(e, 'bindchange');
     },
     bindcolumnchange(e) {
-      console.log(e);
+      const target = e.detail;
+      const column = target.column;
+      const value = target.value;
+      const code = this.data.provinceData[column][value].code;
+      switch (column) {
+        case 0:
+          this.data.cityCode = code;
+          this.cityData(code, 'city');
+          this.data.province[2] = ['请选择'];
+          this.data.provinceData[2] = [];
+          break;
+        case 1:
+          this.data.districtCode = code;
+          this.cityData(code, 'district');
+          break;
+        case 2:
+          break;
+        default:
+          console.log(e, '有问题')
+      }
     },
-    cityData() {
-      /*Promise.all([this.provinceData()]).then((res) => {
-        console.log(res);
-      })*/
-      this.provinceData()
+    cityData(code, type) {
+      return new Promise((resolve, reject) => {
+        findByParentCode({parentCode: code}, (res) => {
+          if (res.data.code === 200) {
+            let data = res.data.object;
+            let city = data.map((item) => {
+              return item.name
+            });
+            if (type === 'city') {
+              this.data.province[1] = city;
+              this.data.provinceData[1] = data;
+              this.setData({
+                province: this.data.province
+              });
+              resolve(data)
+            } else if (type === 'district') {
+              this.data.province[2] = city;
+              this.data.provinceData[2] = data;
+              this.setData({
+                province: this.data.province
+              });
+              resolve(data)
+            }
+          }
+        });
+      })
     },
     provinceData() {
       return new Promise((resolve, reject) => {
@@ -37,6 +79,7 @@ Component({
             let province = data.map((item) => {
               return item.name
             });
+            this.data.provinceData[0] = data;
             this.setData({
               province: [province, ['请选择'], ['请选择']]
             });
@@ -46,36 +89,9 @@ Component({
           }
         })
       }).then((res) => {
-        return new Promise((resolve, reject) => {
-          findByParentCode({parentCode: res[0].code}, (res) => {
-            if (res.data.code === 200) {
-              let data = res.data.object;
-              let city = data.map((item) => {
-                return item.name
-              });
-              this.data.province[1] = city;
-              this.setData({
-                province: this.data.province
-              });
-              resolve(data)
-            }
-          })
-        })
+         return this.cityData(res[0].code, 'city')
       }).then((res) => {
-        findByParentCode({parentCode: res[0].code}, (res) => {
-          let data = res.data.object;
-          let district = data.map((item) => {
-            return item.name
-          });
-          this.data.province[2] = district;
-          this.setData({
-            province: this.data.province
-          });
-        })
-      })
-
-     .catch(() => {
-        console.log('省级数据有问题')
+        this.cityData(res[0].code, 'district')
       })
     }
   }
